4fa9923465bbbf84c025890d68cf400bd4c0d457
Crumbelcatch <maltelantow@protonmail.com>
2024-06-08 20:31:45 +0200

    Yeet schnell

---
+++ Plattformer2024_3A/Source/Plattformer2024_3A/EmptyFucker.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EmptyFucker.h"
+
+EmptyFucker::EmptyFucker()
+{
+}
+
+EmptyFucker::~EmptyFucker()
+{
+}
